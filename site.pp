74c1afc579a3d13ee765b9d363459c0d7f23ab16
Artem Buria <artem_buria@epam.com>
2017-07-04 18:20:33 +0300

    call module

--- site.pp
+++ site.pp
@@ -1,7 +1,14 @@
 $servicename = hiera('ntpname')
+
 package { 'ntp' :
         ensure => installed,
         }
+
 service {$servicename:
           ensure => running,
         }
+
+class {'cr_user':
+  user_name => 'tester',
+  user_password => 'passwd'
+  }
