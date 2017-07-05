b165cd4d76eb9bb54696ed1961f2c794f1ae8049
Artem Buria <artem_buria@epam.com>
2017-07-05 15:35:06 +0300

    hiera update

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,5 @@
 node default{
   $classes = hiera('classes', '')
-  $package = hiera('package', '')
-  $service = hiera('service', '')
-  create_resources('package', $package)
-  create_resources('service', $service)
   if ($classes) {
     hiera_include('classes')
   }
