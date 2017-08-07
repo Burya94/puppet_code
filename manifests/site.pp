bb5266dbc7c0afc68e4c17f6af5704d7a36ccf4c
Artem Buria <artem_buria@epam.com>
2017-08-07 16:14:14 +0300

    u

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,6 +3,8 @@ node default{
   if ($classes) {
     validate_array($classes)
     hiera_include('classes')
+    $logstash_configs = hiera('logstash_configs', {})
+    create_resources('logstash::configfile', $logstash_configs) 
   }
   else{
     notify { 'Default node invocation' :}
