e664dd46a5084be0bcb2039e4ef354e5662af9aa
Artem Buria <artem_buria@epam.com>
2017-08-11 10:51:28 +0300

    zookeeper with exhibitor on ubuntu

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,6 +3,10 @@ node default{
   if ($classes) {
     validate_array($classes)
     hiera_include('classes')
+    $cfg = lookup("configs", Hash)
+    class {'exhibitor':
+      log4j_xml => $cfg,
+    }
   }
   else{
     notify { 'Default node invocation' :}
