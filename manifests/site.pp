925f06949feeaca3a5acae8b224b8eed0318bf9b
Artem Buria <artem_buria@epam.com>
2017-08-30 15:17:19 +0300

    deleted resources

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,21 +1,8 @@
 node default{
-  $packages = ['gcc', 'gcc-c++', 'kernel-devel', 'libxslt-devel', 'libffi-devel', 'openssl-devel', 'git']
   $classes = hiera('classes', '')
   if ($classes) {
-     package { $packages:
-         ensure => 'installed'
-     }
      validate_array($classes)
      hiera_include('classes')
-     $pip_pkgs = hiera('pip_pkgs', {})
-     create_resources('python::pip', $pip_pkgs)
-     $ssh_key = hiera('ssh_file', {})
-     create_resources('file', $ssh_key)
-     $dags = hiera('dags_repo', {})
-     create_resources('vcsrepo', $dags)
-     $exec_commad = hiera('execute', {})
-     create_resources('exec', $exec_commad)
-
   }
   else{
       notify { 'Default node invocation' :}
