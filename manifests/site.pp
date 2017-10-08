9f62b4a15a89066510df6a6f844e15dc02272975
Artem Buria <artem_buria@epam.com>
2017-10-08 20:25:25 +0300

    zk init

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,10 +3,6 @@ node default{
   if ($classes) {
     validate_array($classes)
     hiera_include('classes')
-    $cfg = lookup("configs", Hash)
-    class {'exhibitor':
-      log4j_xml => $cfg,
-    }
   }
   else{
     notify { 'Default node invocation' :}
