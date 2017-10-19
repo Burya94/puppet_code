8eb19d7dc72b1b09580a8a2451b56eaed59ea436
Artem Buria <Artem_Buria@epam.com>
2017-10-19 10:16:41 +0300

    upd for puppetfile and hiera

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,10 +3,10 @@ node default{
   if ($classes) {
     validate_array($classes)
     hiera_include('classes')
-    $logstash_configs = hiera('logstash_configs', {})
-    create_resources('logstash::configfile', $logstash_configs)
-    $logstash_plugins = hiera('logstash_plugins', {})
-    create_resources('logstash::plugin', $logstash_plugins)
+    #$logstash_configs = hiera('logstash_configs', {})
+    #create_resources('logstash::configfile', $logstash_configs)
+    #$logstash_plugins = hiera('logstash_plugins', {})
+    #create_resources('logstash::plugin', $logstash_plugins)
   }
   else{
     notify { 'Default node invocation' :}
