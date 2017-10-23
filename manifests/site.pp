5d8d639d806784b0b4d8c2985b6b765c4a5d0a47
Artem Buria <buriartem263@gmail.com>
2017-10-23 20:25:09 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,10 +3,8 @@ node default{
   if ($classes) {
     validate_array($classes)
     hiera_include('classes')
-    $cfg = lookup("configs", Hash)
-    class {'exhibitor':
-      log4j_xml => $cfg,
-    }
+    $exec_res = hiera("exec", {})
+    create_resources = hiera('Exec', $exec_res)
   }
   else{
     notify { 'Default node invocation' :}
