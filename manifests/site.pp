b9b18c4b0655b31f6c1fedd6539097c5574693dd
Artem Buria <buriartem263@gmail.com>
2017-10-27 14:04:46 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,10 +3,11 @@ node default{
   if ($classes) {
     validate_array($classes)
     hiera_include('classes')
+    }
     exec {'init solr':
       command => "bin/solr start -e cloud -z 172.17.45.6:2181,172.17.45.7:2181,172.17.45.8:2181 -noprompt",
       user => 'root'
-    }
+      }
   }
   else{
     notify { 'Default node invocation' :}
