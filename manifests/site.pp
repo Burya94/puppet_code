0216a7ddda6aa997434657844f2c22beb9a24839
Artem Buria <buriartem263@gmail.com>
2017-10-30 13:50:37 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,6 +3,8 @@ node default{
   if ($classes) {
     validate_array($classes)
     hiera_include('classes')
+    $start = hiera("run", {})
+    create_resources('exec', $start)
   }
   else{
     notify { 'Default node invocation' :}
