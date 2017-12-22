f7562050fabe4d5506e9bb33583648c08f0c2800
Artem Buria <buriartem263@gmail.com>
2017-12-22 19:51:37 +0200

    iptables

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,6 +5,12 @@ node default{
     hiera_include('classes')
     $init = hiera("init", {})
     create_resources('exec', $init)
+    exec { 'iptables':
+      command => "/bin/systemctl stop solr && /sbin/iptables -A INPUT -p tcp -s ${::ipaddress_enp0s8} -j ACCEPT"
+    }->
+    exec { 'iptables out':
+      command => "/sbin/iptables -A OUTPUT -p tcp -d ${::ipaddress_enp0s8} -j ACCEPT"
+    }
   }
   else{
     notify { 'Default node invocation' :}
